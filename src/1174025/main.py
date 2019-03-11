d7a2d67520f1aaecd7f62842e720a800c1811f64
Dezha <dezhamartha17@gmail.com>
2019-03-11 23:41:51 +0700

    maap lama

---
+++ src/1174025/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 11 19:10:18 2019
+
+@author: dezha
+"""
+
+lib = __import__('3lib')
+
+npm = "1174025"
+
+hasil1 = lib.jwbNo1(npm)
+hasil2 = lib.jwbNo2(npm)
+hasil3 = lib.jwbNo3(npm)
+hasil4 = lib.jwbNo4(npm)
+hasil5 = lib.jwbNo5(npm)
+hasil6 = lib.jwbNo6(npm)
+hasil7 = lib.jwbNo7(npm)
+hasil8 = lib.jwbNo8(npm)
+hasil9 = lib.jwbNo9(npm)
+hasil10 = lib.jwbNo10(npm)
+print()
+
+from kelas3lib import kelas3lib
+
+npm = "1174025"
+
+kel3lib = kelas3lib(npm)
+
+kel3lib.JwbNo1()
+kel3lib.JwbNo2()
+kel3lib.JwbNo3()
+kel3lib.JwbNo4()
+kel3lib.JwbNo5()
+kel3lib.JwbNo6()
+kel3lib.JwbNo7()
+kel3lib.JwbNo8()
+kel3lib.JwbNo9()
+kel3lib.JwbNo10()
+
